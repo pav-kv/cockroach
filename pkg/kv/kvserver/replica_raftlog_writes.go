cc9bdcdf2dbf8c9a2037b19a75737ce755df9c3d
Pavel Kalinnikov <pavel@cockroachlabs.com>
2024-12-06 19:19:43 +0000

    kvserver: move log truncation code to log file
    Epic: none
    Release note: none

--- pkg/kv/kvserver/replica_raftlog_writes.go
+++ pkg/kv/kvserver/replica_raftlog_writes.go
@@ -8,7 +8,10 @@ package kvserver
 import (
 	"context"
 
+	"github.com/cockroachdb/cockroach/pkg/kv/kvpb"
+	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/kvserverpb"
 	"github.com/cockroachdb/cockroach/pkg/kv/kvserver/logstore"
+	"github.com/cockroachdb/cockroach/pkg/util/log"
 )
 
 // Raft log storage writes are carried out under raftMu which blocks all other
@@ -94,4 +97,97 @@ func (r *replicaLogStorage) updateLogSize(ctx context.Context) (int64, error) {
 	r.shMu.raftLogLastCheckSize = size
 	r.shMu.raftLogSizeTrusted = true
 	return size, nil
+}
+
+// handleTruncatedStateResult is a post-apply handler for the raft log
+// truncation command. It updates the in-memory state of the Replica with the
+// new RaftTruncatedState and log size delta, and removes obsolete entries from
+// the raft log cache and sideloaded storage.
+//
+// The sideloadIncluded flag specifies whether the raftLogDelta already includes
+// the total size of the sideloaded entries. It is true in loosely coupled
+// truncations stack, and false in the tightly coupled stack. The latter will
+// eventually be removed.
+//
+// The isDeltaTrusted flag specifies whether the raftLogDelta has been correctly
+// computed. The loosely coupled truncations stack sets it to false if, for
+// example, it failed to account for the sideloaded entries. The tightly coupled
+// truncations have correct stats (but excluding the sideloaded entries).
+//
+// TODO(pav-kv): this can be simplified further.
+func (r *Replica) handleTruncatedStateResult(
+	ctx context.Context,
+	t kvserverpb.RaftTruncatedState,
+	expectedFirstIndexPreTruncation kvpb.RaftIndex,
+	raftLogDelta int64,
+	sideloadIncluded bool,
+	isDeltaTrusted bool,
+) {
+	r.raftMu.AssertHeld()
+	// TODO(pav-kv): add unit tests for this logic.
+	expectedFirstIndexWasAccurate :=
+		r.shMu.raftTruncState.Index+1 == expectedFirstIndexPreTruncation
+	isRaftLogTruncationDeltaTrusted := isDeltaTrusted &&
+		(expectedFirstIndexWasAccurate || expectedFirstIndexPreTruncation == 0)
+
+	// TODO(pav-kv): we are updating the truncation state, but leaving the raft
+	// log size at the previous value and update it in a different Replica.mu
+	// section in handleRaftLogDeltaResult. This can confuse the truncations queue
+	// to make decisions based on incorrect stats. We should fix this and other
+	// log stats inconsistencies.
+	r.mu.Lock()
+	r.shMu.raftTruncState = t
+	r.mu.Unlock()
+
+	// Clear any entries in the Raft log entry cache for this range up
+	// to and including the most recently truncated index.
+	r.store.raftEntryCache.Clear(r.RangeID, t.Index+1)
+
+	// Truncate the sideloaded storage. This is safe because the new truncated
+	// state is synced. If it wasn't, a crash right after removing the sideloaded
+	// entries could result in missing entries in the log.
+	log.Eventf(ctx, "truncating sideloaded storage up to (and including) index %d", t.Index)
+	// TODO(#93248): when the legacy caller of handleTruncatedStateResult is
+	// removed, stop calculating the size of the removed files.
+	// TODO(pav-kv): TruncateTo can end up removing leftover files that were
+	// previously compacted out of the log. We don't clean up the leftover files
+	// at start up. So the size computation here can return a larger delta and
+	// lead to inconsistent raft log size tracking.
+	size, _, err := r.raftMu.sideloaded.TruncateTo(ctx, t.Index+1)
+	if err != nil {
+		// We don't *have* to remove these entries for correctness. Log a
+		// loud error, but keep humming along.
+		log.Errorf(ctx, "while removing sideloaded files during log truncation: %+v", err)
+	}
+	// NB: we don't sync the sideloaded entry files removal here for performance
+	// reasons.
+	//
+	// TODO(pav-kv): If a crash occurs between syncing the truncated state and the
+	// TruncateTo removing the files, there will be dangling files at the next
+	// start. We should clean them up at startup.
+
+	if !sideloadIncluded {
+		raftLogDelta -= size
+	}
+	r.handleRaftLogDeltaResult(raftLogDelta, isRaftLogTruncationDeltaTrusted)
+}
+
+// TODO(pav-kv): consolidate with other truncated state updates.
+func (r *Replica) handleRaftLogDeltaResult(delta int64, isDeltaTrusted bool) {
+	r.raftMu.AssertHeld()
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.shMu.raftLogSize += delta
+	r.shMu.raftLogLastCheckSize += delta
+	// Ensure raftLog{,LastCheck}Size is not negative since it isn't persisted
+	// between server restarts.
+	if r.shMu.raftLogSize < 0 {
+		r.shMu.raftLogSize = 0
+	}
+	if r.shMu.raftLogLastCheckSize < 0 {
+		r.shMu.raftLogLastCheckSize = 0
+	}
+	if !isDeltaTrusted {
+		r.shMu.raftLogSizeTrusted = false
+	}
 }
