3993610381736f9c7a99d881c67272233c8ccbbe
Pavel Kalinnikov <pavel@cockroachlabs.com>
2025-10-14 10:40:58 +0100

    kvstorage: abstract separated engines
    Epic: none
    Release note: none

---
+++ pkg/kv/kvserver/kvstorage/engine.go
@@ -0,0 +1,72 @@
+// Copyright 2025 The Cockroach Authors.
+//
+// Use of this software is governed by the CockroachDB Software License
+// included in the /LICENSE file.
+
+package kvstorage
+
+import "github.com/cockroachdb/cockroach/pkg/storage"
+
+// Engine encapsulates the KV storage engine that allows accessing the state
+// machine and raft state. It supports both combined and logically/physically
+// separated engines (depending on whether the underlying storage engines are
+// the same or different).
+type Engine struct {
+	state storage.Engine // state machine engine
+	raft  storage.Engine // raft engine
+	isSep bool           // specifies whether the engines are separated
+}
+
+// MakeEngine returns an instance of Engine.
+func MakeEngine(state, raft storage.Engine) Engine {
+	if state != raft {
+		panic("engines must be the same")
+	}
+	return Engine{state: state, raft: raft}
+}
+
+func MakeSepEngine(state, raft storage.Engine) Engine {
+	return Engine{state: state, raft: raft, isSep: true}
+}
+
+// NewBatch creates a new write batch. By default, the batch only accepts
+// mutation to the state machine. Transactions across the two engines must call
+// AddRaft at least once, to allow writes to the raft engine.
+func (e *Engine) NewBatch() Batch {
+	return Batch{state: e.state.NewBatch()}
+}
+
+// AddRaft extends the batch with the capability of mutating the raft state. In
+// the separated engines mode, the raft batch is distinct from the state machine
+// batch.
+func (e *Engine) AddRaft(b *Batch) {
+	if b.raft != nil {
+		return
+	} else if e.isSep {
+		b.raft = e.raft.NewBatch()
+	} else {
+		b.raft = b.state
+	}
+}
+
+// Batch encapsulates the KV storage engine batch that allows writing to the
+// state machine and (optionally) raft state.
+type Batch struct {
+	state storage.Batch // state machine batch
+	raft  storage.Batch // raft state batch
+}
+
+// Close ends the lifetime of the batch and clears its resources.
+func (b *Batch) Close() {
+	if b.state == nil {
+		return
+	}
+	// Close the raft batch only if it is separated from the state machine batch,
+	// to avoid double Close.
+	if b.raft != nil && b.raft != b.state {
+		b.raft.Close()
+	}
+	b.raft = nil
+	b.state.Close()
+	b.state = nil
+}
