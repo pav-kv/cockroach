9c51e83adacdfa4bc98d75b8a87cf3354fe8a0f4
Pavel Kalinnikov <pavel@cockroachlabs.com>
2026-01-17 02:49:04 +0000

    NewSeparatedIntentScanner

--- pkg/kv/kvserver/replica_rangefeed.go
+++ pkg/kv/kvserver/replica_rangefeed.go
@@ -514,7 +514,8 @@ func (r *Replica) registerWithRangefeedRaftMuLocked(
 		// synchronizes with the rangefeed Processor calling this function by
 		// waiting for the Register call below to return.
 		r.raftMu.AssertHeld()
-		return rangefeed.NewSeparatedIntentScanner(streamCtx, r.store.TODOEngine(), desc.RSpan())
+		// FIXME: is this right?
+		return rangefeed.NewSeparatedIntentScanner(streamCtx, r.store.StateEngine(), desc.RSpan())
 	}
 
 	// NB: This only errors if the stopper is stopping, and we have to return here
